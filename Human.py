689e9d5d409e087af6e0ae24e52be1b43c737fae
Al-Rou <roal.24.alro@gmail.com>
2021-03-17 23:35:16 -0400

    Battle underway3

--- Human.py
+++ Human.py
@@ -1,4 +1,8 @@
+from enum import Enum
+
 class Human(Player):
     def __init__(self, life, mana):
         super().__init__(life)
         self.mana = mana
+
+    Weapon = Enum('Weapon', 'sword fireball')
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from enum import Enum
+
 class dirst:
     def first(self):
         for i in range (10):
             i += 2
             print(i)
 
+    Weapon = Enum('Weapon', 'sword fireball')
 
 a = dirst()
 a.first()
+print(a.Weapon.fireball.name)
+print(a.Weapon.fireball)
