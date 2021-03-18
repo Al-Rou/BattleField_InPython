0a78d465dc08865f421f805471169ed9274ecbb6
Al-Rou <roal.24.alro@gmail.com>
2021-03-17 23:05:41 -0400

    Battle underway1

--- Player.py
+++ Player.py
@@ -2,8 +2,3 @@ class Player:
     def __init__(self, life):
         self.life = life
         self.loseStatus = False
-
-    def getLoseStatus(self):
-        return self.loseStatus
-    def setLoseStatus(self, newStatus):
-        self.loseStatus = newStatus
